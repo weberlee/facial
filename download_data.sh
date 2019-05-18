ebb9e0edcf2143018ba6b65a5835409ce9b7da05
Bruce Yong Li <weberlee@icloud.com>
2019-05-18 16:58:40 +0800

    Create download_data.sh

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir ./data
+wget -P ./data/ https://s3.amazonaws.com/video.udacity-data.com/topher/2018/May/5aea1b91_train-test-data/train-test-data.zip
+unzip -n ./data/train-test-data.zip -d ./data
