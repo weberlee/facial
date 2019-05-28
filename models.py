67e39fbcc7ff3821757318b4524ac9cd5bd0a38d
Bruce Yong Li <weberlee@icloud.com>
2019-05-28 18:29:34 +0800

    Update model

--- models.py
+++ models.py
@@ -19,19 +19,46 @@ class Net(nn.Module):
 
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        # output = (W-F)/S+1 = (224-5)/1+1=220, the output tensor for one image will be (32, 220, 220)
+        #
+        self.conv1 = nn.Conv2d(1, 32, 4)
 
         # maxpool layer
         # pool with kernel_size=2, stride=2
+        # (32, 110, 110)
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.conv2 = nn.Conv2d(10, 20, 3)
+        # dropout p=0.1
+        self.conv1_drop = nn.Dropout(p=0.1)
 
-        self.fc1 = nn.Linear(20*5*5, 50)
+        # output = (W-F)/S +1 = (110-5)/1 +1 = 106
+        self.conv2 = nn.Conv2d(32, 64, 5)
 
-        self.fc1_drop = nn.Dropout(p=0.4)
+        # dropout p=0.2
+        self.conv2_drop = nn.Dropout(p=0.2)
 
-        self.fc2 = nn.Linear(50, 10)
+        # output = (W-F)/S +1 = (54-2)/1 +1 = 53
+        self.conv3 = nn.Conv2d(64, 128, 2)
+
+        # dropout p=0.3
+        self.conv3_drop = nn.Dropout(p=0.3)
+
+        # output = (W-F)/S +1 = (26-1)/1 +1 = 26
+        self.conv4 = nn.Conv2d(128, 256, 1)
+
+        # dropout p=0.4
+        self.conv4_drop = nn.Dropout(p=0.4)
+
+        self.fc1 = nn.Linear(256*13*13, 1000)
+
+        # dropout p=0.5
+        self.fc1_drop = nn.Dropout(p=0.5)
+
+        self.fc2 = nn.Linear(1000, 1000)
+
+        self.fc2_drop = nn.Dropout(p=0.6)
+
+        self.fc3 = nn.Linear(1000, 136)
 
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -43,18 +70,26 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
 
-        # two conv/relu + pool layers
+        # four conv/relu + pool layers
         x = self.pool(F.relu(self.conv1(x)))
+        x = self.conv1_drop(x)
         x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv2_drop(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.conv3_drop(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.conv4_drop(x)
 
         # prep for linear layer
         # this line of code is the equivalent of Flatten in Keras
         x = x.view(x.size(0), -1)
 
-        # two linear layers with dropout in between
+        # three linear layers with dropout in between
         x = F.relu(self.fc1(x))
         x = self.fc1_drop(x)
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc2_drop(x)
+        x = self.fc3(x)
 
         # a modified x, having gone through all the layers of your model, should be returned
         return x
