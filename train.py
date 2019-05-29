09a35d68d776e71de92a692aa20462524fcc174b
Bruce Yong Li <weberlee@icloud.com>
2019-05-29 23:32:38 +0800

    add training args

--- train.py
+++ train.py
@@ -1,3 +1,9 @@
+import argparse
+import datetime
+import json
+import os
+from os.path import join
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,19 +16,88 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from data_tools import DataTools
 
+def parse(args=None):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--attrs', dest='attrs', default=attrs_default, nargs='+', help='attributes to learn')
+    parser.add_argument('--data', dest='data', type=str, choices=['CelebA', 'CelebA-HQ'], default='CelebA')
+    parser.add_argument('--data_path', dest='data_path', type=str, default='data/img_align_celeba')
+    parser.add_argument('--attr_path', dest='attr_path', type=str, default='data/list_attr_celeba.txt')
+    parser.add_argument('--image_list_path', dest='image_list_path', type=str, default='data/image_list.txt')
+
+    parser.add_argument('--img_size', dest='img_size', type=int, default=128)
+    parser.add_argument('--shortcut_layers', dest='shortcut_layers', type=int, default=1)
+    parser.add_argument('--inject_layers', dest='inject_layers', type=int, default=0)
+    parser.add_argument('--enc_dim', dest='enc_dim', type=int, default=64)
+    parser.add_argument('--dec_dim', dest='dec_dim', type=int, default=64)
+    parser.add_argument('--dis_dim', dest='dis_dim', type=int, default=64)
+    parser.add_argument('--dis_fc_dim', dest='dis_fc_dim', type=int, default=1024)
+    parser.add_argument('--enc_layers', dest='enc_layers', type=int, default=5)
+    parser.add_argument('--dec_layers', dest='dec_layers', type=int, default=5)
+    parser.add_argument('--dis_layers', dest='dis_layers', type=int, default=5)
+    parser.add_argument('--enc_norm', dest='enc_norm', type=str, default='batchnorm')
+    parser.add_argument('--dec_norm', dest='dec_norm', type=str, default='batchnorm')
+    parser.add_argument('--dis_norm', dest='dis_norm', type=str, default='instancenorm')
+    parser.add_argument('--dis_fc_norm', dest='dis_fc_norm', type=str, default='none')
+    parser.add_argument('--enc_acti', dest='enc_acti', type=str, default='lrelu')
+    parser.add_argument('--dec_acti', dest='dec_acti', type=str, default='relu')
+    parser.add_argument('--dis_acti', dest='dis_acti', type=str, default='lrelu')
+    parser.add_argument('--dis_fc_acti', dest='dis_fc_acti', type=str, default='relu')
+    parser.add_argument('--lambda_1', dest='lambda_1', type=float, default=100.0)
+    parser.add_argument('--lambda_2', dest='lambda_2', type=float, default=10.0)
+    parser.add_argument('--lambda_3', dest='lambda_3', type=float, default=1.0)
+    parser.add_argument('--lambda_gp', dest='lambda_gp', type=float, default=10.0)
+
+    parser.add_argument('--mode', dest='mode', default='wgan', choices=['wgan', 'lsgan', 'dcgan'])
+    parser.add_argument('--epochs', dest='epochs', type=int, default=200, help='# of epochs')
+    parser.add_argument('--batch_size', dest='batch_size', type=int, default=32)
+    parser.add_argument('--num_workers', dest='num_workers', type=int, default=0)
+    parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='learning rate')
+    parser.add_argument('--beta1', dest='beta1', type=float, default=0.5)
+    parser.add_argument('--beta2', dest='beta2', type=float, default=0.999)
+    parser.add_argument('--n_d', dest='n_d', type=int, default=5, help='# of d updates per g update')
+
+    parser.add_argument('--b_distribution', dest='b_distribution', default='none', choices=['none', 'uniform', 'truncated_normal'])
+    parser.add_argument('--thres_int', dest='thres_int', type=float, default=0.5)
+    parser.add_argument('--test_int', dest='test_int', type=float, default=1.0)
+    parser.add_argument('--n_samples', dest='n_samples', type=int, default=16, help='# of sample images')
+
+    parser.add_argument('--save_interval', dest='save_interval', type=int, default=1000)
+    parser.add_argument('--sample_interval', dest='sample_interval', type=int, default=1000)
+    parser.add_argument('--gpu', dest='gpu', action='store_true')
+    parser.add_argument('--multi_gpu', dest='multi_gpu', action='store_true')
+    parser.add_argument('--experiment_name', dest='experiment_name', default=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
+
+    return parser.parse_args(args)
+
+args = parse()
+print(args)
+
+args.lr_base = args.lr
+args.n_attrs = len(args.attrs)
+args.betas = (args.beta1, args.beta2)
+
+os.makedirs(join('output', args.experiment_name), exist_ok=True)
+os.makedirs(join('output', args.experiment_name, 'checkpoint'), exist_ok=True)
+os.makedirs(join('output', args.experiment_name, 'sample_training'), exist_ok=True)
+with open(join('output', args.experiment_name, 'setting.txt'), 'w') as f:
+    f.write(json.dumps(vars(args), indent=4, separators=(',', ':')))
+
 data_tools = DataTools()
 transformed_training_dataset = data_tools.transformed_training_data
 
 # load training data in batches
-batch_size = 10
+batch_size = args.batch_size
+num_workers = args.num_workers
 train_loader = DataLoader(transformed_training_dataset,
                           batch_size=batch_size,
                           shuffle=True,
-                          num_workers=4)
+                          num_workers=num_workers)
 
 # Define the loss and optimization
+lr = args.lr
 criterion = nn.SmoothL1Loss()
-optimizer = optim.Adam(net.parameters(), lr=0.0005)
+optimizer = optim.Adam(net.parameters(), lr=lr)
 
 def train_net(n_epochs):
 
@@ -70,12 +145,12 @@ def train_net(n_epochs):
     print('Finished Training')
 
 # train your network
-n_epochs = 4 # start small, and increase when you've decided on your model structure and hyperparams
+n_epochs = args.epochs # start small, and increase when you've decided on your model structure and hyperparams
 train_net(n_epochs)
 
 # ## TODO: change the name to something uniqe for each new model
 model_dir = 'saved_models/'
-model_name = 'keypoints_model_1.pt'
+model_name = args.experiment_name+'.pt'
 
 # # after training, save your model parameters in the dir 'saved_models'
 torch.save(net.state_dict(), model_dir+model_name)
